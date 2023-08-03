8bc5dd3c668aec9a5c7a71c51814b776ceb13ecd
unknown <sanchit2005yudavanshi@gmail.com>
2023-08-03 22:25:29 +0530

    done with our navbar

--- pages/components/Navbar.js
+++ pages/components/Navbar.js
@@ -1,9 +1,22 @@
 import React from "react";
+import Link from "next/link";
+import styles from "/styles/basic.module.css";
+import { Inter } from "next/font/google";
 
-function Navbar(){
-    return(
-        <div>navbar</div>
-    )
+const inter = Inter({ subsets: ["latin"] });
+function Navbar() {
+  return (
+    <main className={inter.className}>
+      <nav className={styles.navbar}>
+        <Link className={styles.navLinks} href="/">
+          Home
+        </Link>
+        <Link className={styles.navLinks} href="/posts">
+          Posts
+        </Link>
+      </nav>
+    </main>
+  );
 }
 
 export default Navbar;
